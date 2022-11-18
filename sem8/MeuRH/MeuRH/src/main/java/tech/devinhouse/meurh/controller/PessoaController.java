07f9571910a47c81fe31afabaaed4507db135a64
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-11-17 21:25:03 -0300

    projeto spring

---
+++ sem8/MeuRH/MeuRH/src/main/java/tech/devinhouse/meurh/controller/PessoaController.java
@@ -0,0 +1,15 @@
+package tech.devinhouse.meurh.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pessoas")
+public class PessoaController {
+
+    @GetMapping()
+    public String getPessoas(){
+        return "teste";
+    }
+}
