62e470f52541dad90cad6f6d2d94ab536ed9f7d6
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-11-17 21:42:32 -0300

    buscando por id

--- sem8/MeuRH/MeuRH/src/main/java/tech/devinhouse/meurh/controller/PessoaController.java
+++ sem8/MeuRH/MeuRH/src/main/java/tech/devinhouse/meurh/controller/PessoaController.java
@@ -1,8 +1,11 @@
 package tech.devinhouse.meurh.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.websocket.server.PathParam;
 
 @RestController
 @RequestMapping("/pessoas")
@@ -12,4 +15,10 @@ public class PessoaController {
     public String getPessoas(){
         return "teste";
     }
+
+    @GetMapping("/{id}")
+    public String ByID(@PathVariable("id") String id){
+        return id;
+    }
+
 }
