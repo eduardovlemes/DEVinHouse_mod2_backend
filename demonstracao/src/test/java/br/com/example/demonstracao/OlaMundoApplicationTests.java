0cdb821743ee3710434883aa0093bdf50d550539
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-12-13 11:28:58 -0300

    feat: cria projeto exemplo Spring

---
+++ demonstracao/src/test/java/br/com/example/demonstracao/OlaMundoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.example.demonstracao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlaMundoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
