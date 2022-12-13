b93e2af5ff0021cea251ec06635a9268a20b1479
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-12-13 12:12:35 -0300

    feat: adiciona dependencias

---
+++ magazine-senai-spring-data/src/main/java/org/senai/lab365/magazinesenai/MagazineSenaiApplication.java
@@ -0,0 +1,13 @@
+package org.senai.lab365.magazinesenai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagazineSenaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagazineSenaiApplication.class, args);
+	}
+
+}
---
+++ magazine-senai-spring-data/src/test/java/org/senai/lab365/magazinesenai/MagazineSenaiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.senai.lab365.magazinesenai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagazineSenaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
