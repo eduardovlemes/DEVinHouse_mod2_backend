868508f95d9430eafebb902bf24adf6dc8d81b4d
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-12-13 12:12:59 -0300

    feat: adiciona classe que cria a tabela no banco

---
+++ magazine-senai-spring-data/src/main/java/org/senai/lab365/magazinesenai/models/Produto.java
@@ -0,0 +1,37 @@
+package org.senai.lab365.magazinesenai.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name= "produtos")
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String descricao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
