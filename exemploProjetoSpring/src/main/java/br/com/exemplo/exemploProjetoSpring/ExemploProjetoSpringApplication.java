e4acbcc4a1449bbe73f835d27b79270a7eb0c1c0
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-12-13 10:41:19 -0300

    feat: cria projeto Spring (exemplo)

---
+++ exemploProjetoSpring/src/main/java/br/com/exemplo/exemploProjetoSpring/ExemploProjetoSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.exemplo.exemploProjetoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploProjetoSpringApplication.class, args);
+	}
+
+}
---
+++ exemploProjetoSpring/src/test/java/br/com/exemplo/exemploProjetoSpring/ExemploProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.exemplo.exemploProjetoSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
